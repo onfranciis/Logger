56c2910a369a5d57a5b840d4e981b19ebd950ff9
onfranciis <onukwuf@gmail.com>
2024-06-29 12:52:30 +0100

    feat: create Color util

---
+++ src/logger/utils/Color.ts
@@ -0,0 +1,18 @@
+// ANSI escape codes for text color
+export default {
+  warn: "\x1b[33m",
+  info: "\x1b[32m",
+  verbose: "\x1b[32m",
+  error: "\x1b[31m",
+  reset: "\x1b[0m",
+  white: "\x1b[37m",
+  blue: "\x1b[34m",
+};
+
+export const BgColor = {
+  error: "\x1b[41m",
+  info: "\x1b[42m",
+  verbose: "\x1b[42m",
+  warn: "\x1b[43m",
+  white: "\x1b[47m",
+};
